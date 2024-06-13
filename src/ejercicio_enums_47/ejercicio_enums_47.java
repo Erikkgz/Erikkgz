d694f6aa9a66c65fd00f17d7d3bc4f0b00f1310f
Your Name <you@example.com>
2024-06-13 12:00:45 -0300

    hoy 13-6

--- src/ejercicio_enums_47/ejercicio_enums_47.java
+++ src/ejercicio_enums_47/ejercicio_enums_47.java
@@ -9,38 +9,70 @@ public class ejercicio_enums_47 {
 	public static void main(String[] args) {
 
 		Scanner guardar = new Scanner(System.in);
-
-		int numeroIngresado,numSocio;
 		
-		List<String> lista = new ArrayList<>();
+		List<String> nombresSocio = new ArrayList<>();
 
-		lista.add("Enrique");
-		lista.add("Liliana9");
-		lista.add("Lucas");
-		lista.add("Juan");
-		lista.add("Fiorella");
-		lista.add("Ariel");
-		lista.add("Daiana");
+		nombresSocio.add("Enrique");
+		nombresSocio.add("Liliana");
+		nombresSocio.add("Lucas");
+		nombresSocio.add("Juan");
+		nombresSocio.add("Fiorella");
+		nombresSocio.add("Ariel");
+		nombresSocio.add("Daiana");
 		
-		List<String> mumSocio = new ArrayList<>();
+		
+		List<Integer> numSocio = new ArrayList<>();
 
-		numSocio.add("1011");
-		lista.add("2367");
-		lista.add("8748");
-		lista.add("9121");
-		lista.add("817");
-		lista.add("6423");
-		lista.add("2034");
+	      
+		
+		numSocio.add(1011);
+		numSocio.add(2367);
+		numSocio.add(8748);
+		numSocio.add(9121);
+		numSocio.add(817);
+		numSocio.add(6423);
+		numSocio.add(2034);
    
-		System.out.println("desea borrar o modificar algun elemento	(ingrese '1' para borrar) o (ingrese '2' para modificar algo)");
-		numeroIngresado = guardar.nextInt();
 		
-		System.out.println("para encontrar el individuo ingre un numero de socio :");
-        numeroIngresado = guardar.nextInt();		
+		System.out.println("ingrese una opcion ");
+		System.out.println("1. Borrar");
+		System.out.println("2. Modificar");
 		
-		
-		
-		
+		int opcion = guardar.nextInt();
+
+		if(opcion == 1) {
+	     
+			System.out.println("ingrese un numero a borrar");
+			System.out.println(numSocio); 
+			
+			int numBorrar = guardar.nextInt();
+					
+			int posicionEnLista = numSocio.indexOf(numBorrar);
+	
+			nombresSocio.remove(posicionEnLista);
+			numSocio.remove(posicionEnLista);
+			
+			System.out.println("lista de socios"+numSocio);
+			System.out.println("lista de socios"+nombresSocio);
+			
+		}else if(opcion ==2) {
+			
+			System.out.println("ingrese un numero a modificar");
+			System.out.println(numSocio);
+			
+			int numBorrar = guardar.nextInt();
+			
+			int posicionEnLista = numSocio.indexOf(numBorrar);
+			
+			nombresSocio.remove(posicionEnLista);
+			numSocio.remove(posicionEnLista);
+			
+			System.out.println("lista de socios"+numSocio);
+			System.out.println("lista de socios"+nombresSocio);
+			
+			
+			
+		}
 		
 		
 		
---
+++ src/ejercicio_enums_48/ejercicio_enums_48.java
@@ -0,0 +1,5 @@
+package ejercicio_enums_48;
+
+public class ejercicio_enums_48 {
+
+}
